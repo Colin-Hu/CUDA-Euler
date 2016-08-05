5ce5a316ed2220c1562ee2341d411c80362069de
Colin-Hu <colin.hu@outlook.com>
2016-08-04 21:04:28 -0400

    First pass on library routines
    Got sum to work finally.
    Should be cleaned up later with better use
    of pointers.
    Need to work on parallel bubble sort next.

--- myCudalibs/myCudalib.h
+++ myCudalibs/myCudalib.h
@@ -1,48 +1,15 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-#include "../../myCudalibs/myCudalib.h"
+#define LIBTHREADSPERBLOCK 1024
 
-#define THREADSPERBLOCK 1024
-
-cudaError_t findMultipleWithCuda(int *a, unsigned int size);
-cudaError_t fillWithCuda(int *a, int size, int numvalues);
-cudaError_t checkWithCuda(int *a, int size);
-int findarraySize(int fibmaxval);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-	int i = threadIdx.x;
-	c[i] = a[i] + b[i];
-}
-
-__global__ void findMultipleKernel(int *a)
-{
-	int i = blockIdx.x;
-	if (a[i] % 2 != 0)
-	{
-		a[i] = 0;
-	}
-}
-
-__global__ void fillKernel(int *a)
-{
-	int i = blockIdx.x;
-	int j = blockIdx.y;
-	int idx = j * 900 + i;
-
-	a[idx] = (i+100)*(j+100);
-}
-
-__global__ void checkKernel(int *a, int size)
+__global__ void libcheckKernel(int *a, int size)
 {
 	int i = blockIdx.x;
 	int j = threadIdx.x;
-	int idx = i*THREADSPERBLOCK + j;
+	int idx = i*LIBTHREADSPERBLOCK + j;
 	if (idx < size)
 	{
 		int myval = a[idx];
@@ -81,80 +48,42 @@ __global__ void checkKernel(int *a, int size)
 	}
 }
 
-int main()
+__global__ void replaceKernel(int *a, int size)
 {
-	int arraySize;
-	int fibmaxval = 4000000;
-
-	int *products; //Products of 2 three digit numbers
-	const int numvalues = 900;
-	const int numproducts = numvalues * numvalues; //100 - 999
-	products = (int *)malloc(sizeof(int)*numproducts); 
-	
-	cudaError_t cudaStatus = fillWithCuda(products, numproducts, numvalues);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceReset failed!");
-		return 1;
-	}
-
-	printf("First couple %d %d\n",products[0],products[1]);
-
-	//cudaStatus = checkWithCuda(products, numproducts);
-	cudaStatus = checkWithCudalib(products, numproducts);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceReset failed!");
-		return 1;
-	}
-
-	printf("First couple %d %d\n", products[0], products[1]);
-
-	int maxpalindrome = 0;
-	int numpalindrome = 0;
-
-	for(int i = 0; i < numproducts; i++)
+	int i = blockIdx.x;
+	int j = threadIdx.x;
+	int idx = i*LIBTHREADSPERBLOCK + j;
+	if (idx < size)
 	{
-		if (products[i] > 0)
+		if (a[idx] > 0)
 		{
-			numpalindrome += 1;
-		}
-		if (maxpalindrome < products[i])
-		{
-			maxpalindrome = products[i];
-			printf("Max palindrome %d\n", maxpalindrome);
+			a[idx] = 1;
 		}
 	}
-	printf("Num palindrome %d\n", numpalindrome);
 
-	test();
-
-	//cudaStatus = sumWithmyCuda(products, numproducts);
-	//printf("Total: %d\n", myCuda_sum(products, numproducts));
-	int totalsum = myCuda_sum(products, numproducts);
-
-	return 0;
 }
 
-int findarraySize(int fibmaxval)
+__global__ void libsumKernel(int *a, int *b, int size)
 {
-	int indx = 2;
-	int prev1 = 2;
-	int prev2 = 1;
-	int currentval = 0;
-
-	while (currentval < fibmaxval)
-	{
-		indx += 1;
-		currentval = prev1 + prev2;
-		prev2 = prev1;
-		prev1 = currentval;
-		printf("%d,%d\n", indx, currentval);
+	int i = blockIdx.x;
+	int j = threadIdx.x;
+	int idx = i*LIBTHREADSPERBLOCK + j;
+	if ((idx-1) < size/2){
+		b[idx] = a[2*idx]+a[2*idx+1];
 	}
-	return indx - 1;
+	else if (size % 2 == 1 && idx == (size/2)){
+		printf("I am thread %d working on size %d moving %d\n",idx,size,a[size-1]);
+		b[idx] = a[size-1];
+	}
 }
 
+void test()
+{
+	printf("Testlib\n");
+}
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t findMultipleWithCuda(int *a, unsigned int size)
+// Helper function for using CUDA to check for condition
+cudaError_t checkWithCudalib(int *a, int size)
 {
 	int *dev_a = 0;
 	cudaError_t cudaStatus;
@@ -180,65 +109,9 @@ cudaError_t findMultipleWithCuda(int *a, unsigned int size)
 	}
 
 	// Launch a kernel on the GPU with one thread for each element.
-	findMultipleKernel << <size, 1 >> >(dev_a);
-
-	// Check for any errors launching the kernel
-	cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		goto Error;
-	}
-
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
-	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-		goto Error;
-	}
-
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(a, dev_a, size * sizeof(int), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-Error:
-	cudaFree(dev_a);
-
-	return cudaStatus;
-}
-
-// Helper function for computing array
-cudaError_t fillWithCuda(int *a, int size, int numvalues)
-{
-	int *dev_a = 0;
-	cudaError_t cudaStatus;
-
-	// Choose which GPU to run on, change this on a multi-GPU system.
-	cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-		goto Error;
-	}
-	// Allocate GPU buffers for three vectors (two input, one output)    .
-	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	const dim3 blockSize(numvalues, numvalues, 1);
-	// Launch a kernel on the GPU with one thread for each element.
-	fillKernel << <blockSize, 1 >> >(dev_a);
+	printf("Block Size %d\n", size); fflush(stdout);
+	int numBlocks = size / LIBTHREADSPERBLOCK + 1;
+	libcheckKernel << < numBlocks, LIBTHREADSPERBLOCK >> >(dev_a, size);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
@@ -269,7 +142,7 @@ Error:
 }
 
 // Helper function for using CUDA to check for condition
-cudaError_t checkWithCuda(int *a, int size)
+cudaError_t replaceWithCudalib(int *a, int size)
 {
 	int *dev_a = 0;
 	cudaError_t cudaStatus;
@@ -296,8 +169,8 @@ cudaError_t checkWithCuda(int *a, int size)
 
 	// Launch a kernel on the GPU with one thread for each element.
 	printf("Block Size %d\n", size); fflush(stdout);
-	int numBlocks = size / THREADSPERBLOCK + 1;
-	checkKernel << < numBlocks , THREADSPERBLOCK >> >(dev_a, size);
+	int numBlocks = size / LIBTHREADSPERBLOCK + 1;
+	replaceKernel << < numBlocks, LIBTHREADSPERBLOCK >> >(dev_a, size);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
@@ -325,4 +198,107 @@ Error:
 	cudaFree(dev_a);
 
 	return cudaStatus;
+}
+
+// Helper function for using CUDA to check for condition
+int sumWithmyCuda(int *inarray, int size, int *outarray)
+{
+	int *dev_in = 0;
+	int *dev_out = 0;
+	cudaError_t cudaStatus;
+
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+	// Allocate GPU buffers for input array.
+	cudaStatus = cudaMalloc((void**)&dev_in, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	int outsize = ceil(float(size)/2);
+	printf("insize %d outsize %d\n", size, outsize);
+	// Allocate GPU buffers for input array.
+	cudaStatus = cudaMalloc((void**)&dev_out, outsize * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	// Copy input vectors from host memory to GPU buffers.
+	cudaStatus = cudaMemcpy(dev_in, inarray, size * sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	// Launch a kernel on the GPU with one thread for each element.
+	int numBlocks = size / LIBTHREADSPERBLOCK + 1;
+	libsumKernel << < numBlocks, LIBTHREADSPERBLOCK >> >(dev_in, dev_out, size);
+
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	free(outarray);
+	outarray = (int *)malloc(sizeof(int)*outsize);
+
+	// Copy output vector from GPU buffer to host memory.
+	cudaStatus = cudaMemcpy(outarray, dev_out, outsize * sizeof(int), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	int *finalresult;
+	finalresult = (int *)malloc(sizeof(int) * 1);
+	if (outsize > 1)
+	{
+		return sumWithmyCuda(outarray, outsize, finalresult);
+	}
+	else
+	{
+		return *outarray;
+	}
+Error:
+	cudaFree(dev_in);
+
+}
+
+int myCuda_sum(int *a, int size)
+{
+	cudaError_t cudaStatus;
+	int *copy;
+	int *finalsum;
+
+	copy = (int *)malloc(sizeof(int)*size);
+	copy = a;
+
+	finalsum = (int *)malloc(sizeof(int) * 1);
+	finalsum = 0;
+	int finalval = 0;
+	cudaStatus = replaceWithCudalib(copy, size);
+	finalval = sumWithmyCuda(copy, size, finalsum);
+	printf("Finalval %d\n", finalval);
+
+	//int finalsum = copy[0];
+	//free(copy);
+
+	//return finalsum;
+	return 0;
 }
